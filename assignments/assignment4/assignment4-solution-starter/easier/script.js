17c3c6f09a79a1c41f7345ef890d8387827010ba
kishomo4 <kishomo4@gmail.com>
2026-04-22 20:39:47 +0500

    script.js

--- assignments/assignment4/assignment4-solution-starter/easier/script.js
+++ assignments/assignment4/assignment4-solution-starter/easier/script.js
@@ -1,24 +1,5 @@
-// STEP 1: Создаём массив имён
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// STEP 2: Создаём два объекта (библиотеки) для приветствия и прощания
-(function(window) {
-  var helloSpeaker = {};
-  helloSpeaker.speak = function(name) {
-    console.log("Hello " + name);
-  };
-  window.helloSpeaker = helloSpeaker;
-})(window);
-
-(function(window) {
-  var byeSpeaker = {};
-  byeSpeaker.speak = function(name) {
-    console.log("Goodbye " + name);
-  };
-  window.byeSpeaker = byeSpeaker;
-})(window);
-
-// STEP 3: Основной цикл
 for (var i = 0; i < names.length; i++) {
   var name = names[i];
   var firstLetter = name.charAt(0).toLowerCase();
